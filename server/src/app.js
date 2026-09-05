0db07455b5a9b6d01c263a5f81fe14a59f58c28e
andresbisio <113863030+AndresBisio@users.noreply.github.com>
2026-09-05 17:11:48 -0300

    Se implementa autenticacion con token

--- server/src/app.js
+++ server/src/app.js
@@ -2,15 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import clientesRoutes from './routes/clientes.routes.js';
 import inmueblesRoutes from './routes/inmuebles.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import { authMiddleware, requireRole } from './middleware/authMiddleware.js';
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Endpoint de prueba / Health check
-app.use('/api/clientes', clientesRoutes);
-app.use('/api/inmuebles', inmueblesRoutes);
+app.use('/auth', authRoutes);
+
+app.use(authMiddleware);
+
+app.use('/api/clientes', requireRole(['admin']), clientesRoutes);
+app.use('/api/inmuebles', requireRole(['admin']), inmueblesRoutes);
 
 export default app;
